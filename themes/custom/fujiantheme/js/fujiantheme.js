5e4a83a826311a27f4f05873ebaa5147d9c8f373
ubuntu <cwm1021@gmail.com>
2025-02-05 23:40:20 +0800

    custom theme dir

---
+++ themes/custom/fujiantheme/js/fujiantheme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * fujiantheme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.fujiantheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
